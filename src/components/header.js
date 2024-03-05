061affcd674dfd0a49d8501ca02202564286c99f
Alex Michalsky <michalsky@env.kiev.ua>
2024-03-05 12:22:26 +0100

    fix layout and optimize images

--- src/components/header.js
+++ src/components/header.js
@@ -2,125 +2,106 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-const Header = ({ lositeTitle }) => (
-  <header
-    style={{
-      margin: `0 auto`,
-      padding: `var(--space-4) var(--size-gutter)`,
-      display: `flex`,
-      alignItems: `center`,
-      justifyContent: `space-between`,
-      position: `fixed`,
-    }}
-  >
-    <Link
-      to="/"
-      style={{
-        fontSize: `var(--font-sm)`,
-        textDecoration: `none`,
-      }}
-    >
+const Header = ({ siteTitle }) => (
+  <header className="flex flex-col items-center justify-between fixed w-full mx-auto p-4">
+    <Link to="/" className="text-sm no-underline mb-3">
       <StaticImage
         src="../images/logo.png"
-        loading="eager"
+        loading="lazy"
         width={250}
         quality={95}
         formats={["auto", "webp", "avif"]}
         alt=""
-        style={{ marginBottom: `var(--space-3)` }}
       />
     </Link>
 
-    <br />
+    <div className="flex flex-row justify-between w-full">
+      <Link
+        to="https://konstantinunwohl.bandcamp.com/"
+        className="text-sm no-underline mb-3"
+      >
+        <StaticImage
+          src="../images/musik.png"
+          loading="lazy"
+          width={150}
+          quality={95}
+          formats={["auto", "webp", "avif"]}
+          alt=""
+        />
+      </Link>
 
-    <Link
-      to="https://konstantinunwohl.bandcamp.com/"
-      style={{
-        fontSize: `var(--font-sm)`,
-        textDecoration: `none`,
-      }}
-    >
-      <StaticImage
-        src="../images/musik.png"
-        loading="eager"
-        width={150}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-    </Link>
+      <Link
+        to="/lyrics"
+        style={{
+          fontSize: `var(--font-sm)`,
+          textDecoration: `none`,
+        }}
+      >
+        <StaticImage
+          src="../images/lyrics.png"
+          className="text-sm no-underline mb-3"
+          loading="lazy"
+          width={150}
+          quality={95}
+          formats={["auto", "webp", "avif"]}
+          alt=""
+          style={{ marginBottom: `var(--space-3)` }}
+        />
+      </Link>
 
-    <Link
-      to="/lyrics"
-      style={{
-        fontSize: `var(--font-sm)`,
-        textDecoration: `none`,
-      }}
-    >
-      <StaticImage
-        src="../images/lyrics.png"
-        loading="eager"
-        width={150}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-    </Link>
+      <Link
+        to="/kontakt"
+        style={{
+          fontSize: `var(--font-sm)`,
+          textDecoration: `none`,
+        }}
+      >
+        <StaticImage
+          src="../images/kontakt.png"
+          loading="lazy"
+          width={150}
+          quality={95}
+          formats={["auto", "webp", "avif"]}
+          alt=""
+          style={{ marginBottom: `var(--space-3)` }}
+        />
+      </Link>
 
-    <Link
-      to="/kontakt"
-      style={{
-        fontSize: `var(--font-sm)`,
-        textDecoration: `none`,
-      }}
-    >
-      <StaticImage
-        src="../images/kontakt.png"
-        loading="eager"
-        width={150}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-    </Link>
-
-    <Link
-      to="/konzerte"
-      style={{
-        fontSize: `var(--font-sm)`,
-        textDecoration: `none`,
-      }}
-    >
-      <StaticImage
-        src="../images/konzerte.png"
-        loading="eager"
-        width={150}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-    </Link>
-    <Link
-      to="/projekte"
-      style={{
-        fontSize: `var(--font-sm)`,
-        textDecoration: `none`,
-      }}
-    >
-      <StaticImage
-        src="../images/projekte.png"
-        loading="eager"
-        width={150}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-    </Link>
+      <Link
+        to="/konzerte"
+        style={{
+          fontSize: `var(--font-sm)`,
+          textDecoration: `none`,
+        }}
+      >
+        <StaticImage
+          src="../images/konzerte.png"
+          loading="lazy"
+          width={150}
+          quality={95}
+          formats={["auto", "webp", "avif"]}
+          alt=""
+          style={{ marginBottom: `var(--space-3)` }}
+        />
+      </Link>
+      <Link
+        to="/projekte"
+        style={{
+          fontSize: `var(--font-sm)`,
+          textDecoration: `none`,
+        }}
+      >
+        <StaticImage
+          src="../images/projekte.png"
+          loading="lazy"
+          width={150}
+          quality={95}
+          formats={["auto", "webp", "avif"]}
+          alt=""
+          style={{ marginBottom: `var(--space-3)` }}
+        />
+      </Link>
+    </div>
   </header>
 )
 
