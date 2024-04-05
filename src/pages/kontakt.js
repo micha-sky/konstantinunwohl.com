85956671dd02aeb7f8d53560606ce2e445c83466
Alex Michalsky <michalsky@env.kiev.ua>
2024-04-05 23:34:50 +0200

    Update kontakt.js

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -85,24 +85,24 @@ const Kontakt = () => (
         <FontAwesomeIcon icon={faYoutube} /> Youtube
       </a>
     </div>
-
     <div className={styles.textCenter}>
-    <form name="contact" method="POST" data-netlify="true">
-  <input type="hidden" name="subject" 
-  value="Message frm a fan" />
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form></div>
+  <form name="contact" method="POST" data-netlify="true">
+    <input type="hidden" name="form-name" value="contact" />
+    <input type="hidden" name="subject" value="Message frm a fan" />
+    <p>
+      <label>Your Name: <input type="text" name="name" /></label>
+    </p>
+    <p>
+      <label>Your Email: <input type="email" name="email" /></label>
+    </p>
+    <p>
+      <label>Message: <textarea name="message"></textarea></label>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
+</div>
   </Layout>
 )
 
