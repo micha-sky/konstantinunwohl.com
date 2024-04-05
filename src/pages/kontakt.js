473ff21dfdcfec42717c5867700f7f04482b1010
Alex Michalsky <michalsky@env.kiev.ua>
2024-04-05 23:10:09 +0200

    Update contact form layout and functionality

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -85,21 +85,24 @@ const Kontakt = () => (
         <FontAwesomeIcon icon={faYoutube} /> Youtube
       </a>
     </div>
-    <form name="contact" netlify>
-      <p>
-        <label>
-          Name <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Email <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+
+    <div className={styles.textCenter}>
+    <form name="contact" method="POST" data-netlify="true">
+  <input type="hidden" name="subject" 
+  value="Message frm a fan" />
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form></div>
   </Layout>
 )
 
