d9dcb06a89b43ffd993d716b98b14dcde06dbbbf
Alex Michalsky <michalsky@env.kiev.ua>
2024-04-08 11:25:15 +0200

    Update contact form layout and functionality

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -1,11 +1,9 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faInstagram,
   faBandcamp,
   faFacebook,
-  faTwitter,
   faYoutube,
   faSpotify,
   faSoundcloud,
--- src/pages/lyrics.js
+++ src/pages/lyrics.js
@@ -13,7 +13,6 @@ const Lyrics = ({ data }) => {
   return (
     <Layout>
       <div className="flex flex-col main-container">
-        l
         <div className="flex flex-row space-x-0 lyrics-container">
           <div className={styles.coverPlace}>
             <StaticImage
@@ -30,85 +29,380 @@ const Lyrics = ({ data }) => {
           <div className={styles.tracklist}>
             <ol className="list-decimal list-inside">
               <li>
-                <Link to="#ichhasse">ich hass es, wenn man spass hat</Link>
+                <Link to="#ichhasse">Ich hass es, wenn man Spaß hat</Link>
               </li>
               <li>
-                <Link to="#bruecke">ich liebe es, wenn man spass hat</Link>
+                <Link to="#anleitung">Anleitung zum Versagen</Link>
               </li>
-              <li>track3</li>
+              <li>
+                <Link to="#balance">Balance</Link>
+              </li>
               <li>track4</li>
               <li>track5</li>
             </ol>
           </div>
         </div>
-        <div id="ichhasse" className="text-container space-x-0 ml-14">
-          Strophe 1:
+        <div id="ichhasse" className="text-container space-x-0 ml-14 mb-8">
+          <div className="text-2xl bold">Ich hass es, wenn man Spaß hat</div>
+          An der Holstenstraße fährt der letzte Zug
           <br />
-          Ich lauf' durch die Straßen, allein in der Nacht,
+          und ich falle in Gedanken auf ie Gleise
           <br />
-          überall Lachen, doch mein Herz schwer gemacht.
+          ich dachte eigentlich ich hätt genug gehabt
           <br />
-          Sie tanzen und singen, ohne Sorgen, so frei,
-          <br />
-          ich wünscht', ich könnt' auch, doch etwas hält mich dabei.
-          <br />
-          <br />
-          Refrain:
-          <br />
-          Ich hass' es, wenn man Spaß hat, und ich steh' nur am Rand,
-          <br />
-          gefangen in Gedanken, in meinem eigenen Land.
-          <br />
-          Ich hass' es, wenn die Lichter so hell und bunt scheinen,
-          <br />
-          während in meiner Welt nur Wolken erscheinen.
+          am Bahnhof wie zuhause immer das gleiche
           <br />
           <br />
-          Strophe 2:
+          ich gehe meinen Weg in Richtung hier und da
           <br />
-          Sie sagen, komm doch, lass dich einfach mal geh'n,
+          ich weiß schon lange nicht mehr, wo ich gestern war
           <br />
-          doch in ihren Augen kann ich die Wahrheit seh'n.
+          die Tage zieht vorbei, nur die leere Hülle bleibt
           <br />
-          Sie verstehen nicht, wie schwer es ist, zu sein wie ich,
+          im andern zimmer wird gelacht
           <br />
-          immer außerhalb, im Schatten, fern vom Licht.
-          <br />
-          <br />
-          <div id="bruecke">
-            Brücke:
-            <br />{" "}
-          </div>
-          Doch vielleicht ist es Zeit, zu lernen und zu versteh'n,
-          <br />
-          dass auch im Dunkeln Sterne wunderschön.
-          <br />
-          Vielleicht ist es nicht Hass, sondern Neid oder Angst,
-          <br />
-          die mich hält gefangen, in diesem traurigen Tanz.
+          wenn man Spaß hat
           <br />
           <br />
-          Refrain:
+          Ich hass es, wenn man Spaß hat
           <br />
-          Ich hass' es, wenn man Spaß hat, und ich steh' nur am Rand,
+          Wenn man
           <br />
-          gefangen in Gedanken, in meinem eigenen Land.
+          Ich hass es, wenn man Spaß hat
           <br />
-          Aber vielleicht, ja vielleicht, find' ich meinen eigenen Glanz,
+          Ich hass es, wenn man Spaß hat
           <br />
-          lerne zu tanzen, im Regen, beginne meinen Tanz.
+          Wenn man
           <br />
           <br />
-          Outro:
+          Seitdem ich mich kenne
           <br />
-          Also schau' ich hinauf, in die Nacht, so klar,
+          komm ich jedes Jahr im Winter auf dieselben Gedanken
           <br />
-          und finde dort Hoffnung, sie war immer da.
+          die Lebenden sind tot
           <br />
-          Es ist okay zu sein, wer ich bin, mit all meinem Schmerz,
+          und die toten, die leben
           <br />
-          denn auch in der Dunkelheit, findet das Licht seinen Platz.
+          geben Geld aus und gehen wandern
           <br />
+          <br />
+          oh es windet sich im Bett ein Körper
+          <br />
+          den ich besaß
+          <br />
+          oh ich habe viele Bücher, worin ich davon las
+          <br />
+          <br />
+          und wenn der Überdruss
+          <br />
+          der Weile nach mir greift
+          <br />
+          wenn ich alles vernein
+          <br />
+          da bin ich daheim
+          <br />
+          die Funken singen im Zaun
+          <br />
+          wenn man nicht mehr weiß, wofür man zu bleiben braucht
+          <br />
+          wenn ich alles vernein
+          <br />
+          da bin ich daheim
+          <br />
+          <br />
+          bleib bevor du gehst, aber geh bevor du fährst
+          <br />
+          schau zu lange in den Spiegel und du siehst nicht mehr dich selbst
+          <br />
+          das sage dem, der Sorgen trägt
+          <br />
+          und dass die Sorgen immer dauern
+          <br />
+          wenn die Freude auch verweht
+          <br />
+          <br />
+          Jedes Abenteuer ist nur ein ironischer Besuch
+          <br />
+          sowieso ist es zu früh, oder zu spät für den Versuch
+          <br />
+          schon viel davon gehört, vielleicht versuch ich mich daran
+          <br />
+          fall langsam auseinander und was übrig bleibt ist Angst
+          <br />
+          <br />
+          und selbst wenn und selbst wenn und selbst wenn und selbst wenn
+          <br />
+          solang ich alles vernein
+          <br />
+          da bin ich daheim
+          <br />
+          die Funken singen im Zaun
+          <br />
+          wenn man nicht mehr weiß, wofür man zu bleiben braucht
+          <br />
+          wenn ich alles vernein
+          <br />
+          da bin ich daheim
+          <br />
+        </div>
+        <div id="anleitung" className="text-container space-x-0 ml-14">
+          <div className="text-2xl bold">Anleitung zum Versagen </div>
+          Du musst langsam gehen beim schleppen <br />
+          denn du musst hören können
+          <br />
+          wie die Schritte auf der Treppe langsam verhallen
+          <br />
+          <br />
+          <br />
+          immer noch fest dran drauf dran lebendig zu verfallen
+          <br />
+          mancher stirbt an Lebenslust
+          <br />
+          andere am Leben <br />
+          trinke ein Bier, ein Schnaps, schäm dich, denk dumm gemacht
+          <br />
+          dumm gemacht
+          <br />
+          <br />
+          <br />
+          maße dir die Kleider der Wahrheit an
+          <br />
+          denn was auch immer diese nutzlosen Hände vollbracht haben
+          <br />
+          hält nicht lang
+          <br />
+          <br />
+          sieh die Einsamkeit der Welt, aber frag nicht nach
+          <br />
+          ja studiere keine Quellen mehr, nur noch Kommentar
+          <br />
+          und du wirst irgendwann vergessen haben
+          <br />
+          ob es immer schon so war
+          <br />
+          immer schon so war
+          <br />
+          <br />
+          <br />
+          und dann fühle dich
+          <br />
+          und fühle dich fremd
+          <br />
+          und dann fühle dich fremd
+          <br />
+          und dann fühle dich fremd fühl dich fremd fühl dich fremd
+          <br />
+          ja dann fühle dich fremd
+          <br />
+          dann fühle dich fremd
+          <br />
+          dann fühle dich fremd
+          <br />
+          ja dann fühle dich fremd fühl dich fremd fühl dich fremd
+          <br />
+          <br />
+          <br />
+          zwischen quälend ungewissen Entitäten
+          <br />
+          ständig falsch gesetzten Prioritäten
+          <br />
+          allen diesen schambefleckten Zeiten
+          <br />
+          und allen diesen Unzulänglichkeiten
+          <br />
+          genau da da da da da da da da
+          <br />
+          <br />
+          <br />
+          wenn ein Stein fallen will
+          <br />
+          gib ihm besser einen Stoß
+          <br />
+          es gibt nie lang Befreiung <br />
+          aus den Klauen des Tods
+          <br />
+          werd zum Schatten der Bekannten
+          <br />
+          wart wie Wiesen auf den Schnitter
+          <br />
+          werd die Vase deren Blumen auf den Abfall harrn
+          <br />
+          <br />
+          <br />
+          ja fürchte nicht die Höhen
+          <br />
+          aber fürchte den Reflex
+          <br />
+          fürcht die Sehnsucht deines Körpers <br />
+          und die Leute um dich rum, die sagen Pech Pech Pech Pech Pech
+          <br />
+          <br />
+          <br />
+          Lad ein, zur Besichtigung der inneren Unruhe
+          <br />
+          gib auf, wenn die bestätigenden Stimmen verstummen
+          <br />
+          frag nie, ob es noch irgendetwas anderes gibt
+          <br />
+          als das, was zwischen deiner und der Weltenleere liegt
+          <br />
+          <br />
+          <br />
+          sieh die Wolken ein fallender Mann
+          <br />
+          der über den Himmel zieht
+          <br />
+          und dann fühle dich fühle dich fremd <br />
+          fühle dich fremd
+          <br />
+          ja dann fühle dich fremd
+          <br />
+          dann fühle dich fremd
+          <br />
+          ja dann fühle dich fremd fühl dich fremd fühl dich fremd fühl dich
+          fremd
+          <br />
+          dann fühle dich fremd
+          <br />
+          ja dann fühle dich fremd
+          <br />
+          und dann fühle dich fremd
+          <br />
+          ja dann fühle dich fremd fühl dich fremd fühl dich fremd fühl dich
+          fremd
+          <br />
+          <br />
+          zwischen quälend ungewissen Entitäten
+          <br />
+          ständig falsch gesetzten Prioritäten
+          <br />
+          allen diesen schambefleckten Zeiten
+          <br />
+          und allen diesen Unzulänglichkeiten
+          <br />
+          genau da da da da da da da da
+          <br />
+        </div>
+        <div id="balance" className="text-container space-x-0 ml-14 mb-8">
+          <div className="text-2xl bold">Balance</div>
+          <p>
+            Ich z&auml;hle wieder meine Schritte
+            <br />
+            wie Geburtstage und alte Geschichten
+            <br />
+            ich hab viel gesehen, doch die Leute in der Werbung
+            <br />
+            die machen mich nerv&ouml;s
+          </p>
+
+          <p>
+            ja an Tagen wie diesen, sei lieber kriminell
+            <br />
+            so stehl ich abends heimlich Blumen auf dem Weg
+            <br />
+            es ist gut wenn niemand sieht, wie man sich bewegt
+            <br />
+            denn es geht hier um Balance
+          </p>
+
+          <p>
+            Balance
+            <br />
+            es geht hier um Balance
+            <br />
+            Balance
+          </p>
+
+          <p>
+            ich kann mit Silberfischen sprechen
+            <br />
+            und mit &auml;hnlichem Gesocks
+            <br />
+            wir k&ouml;nnten in Symbiose leben
+            <br />
+            aber ich entscheide mich dagegen
+          </p>
+
+          <p>
+            oh man darf nicht viel daran denken
+            <br />
+            wenn man nicht wei&szlig;, woran man ist
+            <br />
+            denn wenn der Wind sich zu oft dreht
+            <br />
+            hilft es dir nichts mehr, wenn du gehst
+          </p>
+
+          <p>
+            Und so schlaf ich im Vorraum der Stille
+            <br />
+            da ruht es sich halb so gut aus
+            <br />
+            ich warf mich in vielerlei Dinge
+            <br />
+            und jetzt komm ich da nicht mehr gut raus
+            <br />
+            Ich schlafe im Vorraum der Stille
+            <br />
+            ein Ort, der zum Enden nichts taugt
+            <br />
+            ich sch&auml;m mich sogar f&uuml;r die Dinge
+            <br />
+            an die man schon lang nicht mehr glaubt
+          </p>
+
+          <p>es geht hier um Balance</p>
+
+          <p>
+            Ich habe eine Schw&auml;che f&uuml;r Phantome
+            <br />
+            sie machen alles so spannend
+            <br />
+            nur man gew&ouml;hnt sich auch zu schnell daran
+            <br />
+            dass Enth&uuml;llung nur entt&auml;uschen kann
+            <br />
+            poetisches Bereuen
+            <br />
+            das macht sich immer gut
+            <br />
+            denn um &uuml;berhaupt zu handeln fehlt dann doch der Mut
+          </p>
+
+          <p>
+            und eines sch&ouml;nen Tages
+            <br />
+            h&ouml;rt man auf davon zu reden
+            <br />
+            geschafft davon, sich selbst zu h&ouml;ren
+            <br />
+            und alles zu verstehen
+            <br />
+            man wird das Gegenteil der andern
+            <br />
+            die man nicht mal gehen sah
+            <br />
+            und man nagt am Gnadenbrot <br />
+            der Fotoalben jedes Jahr
+          </p>
+
+          <p>
+            Und so schlaf ich im Vorraum der Stille
+            <br />
+            da ruht es sich halb so gut aus
+            <br />
+            ich warf mich in vielerlei Dinge
+            <br />
+            und jetzt komm ich da nicht mehr gut raus
+            <br />
+            Ich schlafe im Vorraum der Stille
+            <br />
+            ein Ort, der zum Enden nichts taugt
+            <br />
+            ich sch&auml;m mich sogar f&uuml;r die Dinge
+            <br />
+            an die man schon l&auml;ngst nicht mehr glaubt
+          </p>
         </div>
       </div>
     </Layout>
