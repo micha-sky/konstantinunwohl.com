b9ba83f229992defbc1c5ea665d50b3d8227f06c
Alex Michalsky <michalsky@env.kiev.ua>
2024-04-15 12:18:22 +0200

    Update lyrics.js

--- src/pages/lyrics.js
+++ src/pages/lyrics.js
@@ -37,8 +37,15 @@ const Lyrics = ({ data }) => {
               <li>
                 <Link to="#balance">Balance</Link>
               </li>
-              <li>track4</li>
-              <li>track5</li>
+              <li>
+                <Link to="#rekrut">Eine Träne kein Rekrut</Link>
+              </li>
+              <li>
+                <Link to="#fenster">Fenster zur Welt</Link>
+              </li>
+              <li>
+                <Link to="#wagen">Hoch auf dem gelben Wagen</Link>
+              </li>
             </ol>
           </div>
         </div>
@@ -404,6 +411,192 @@ const Lyrics = ({ data }) => {
             an die man schon l&auml;ngst nicht mehr glaubt
           </p>
         </div>
+        <div id="rekrut" className="text-container space-x-0 ml-14 mb-8">
+          <div className="text-2xl bold">Eine Träne kein Rekrut</div>
+
+          <p>
+            Heute ist ein sch&ouml;ner Tag die Sonne scheint sehr unbedarft die
+            Rennr&auml;der der Stadt sammeln Staub
+          </p>
+
+          <p>
+            du wolltest einkaufen gehen f&uuml;r eine nette Kleinigkeit so oft
+            hast dus versucht aber kamst damit nie weit
+          </p>
+
+          <p>
+            wirf dich mitten ins Get&uuml;mmel deren Ziele du nicht teilst zieh
+            dir eine kleine M&uuml;tze an dann blendest du dich ein
+          </p>
+
+          <p>
+            Vergangenheit ist zur Strafe vorbei du musst in Zeiten der
+            Verwirrung nur mit dir im reinen sein
+          </p>
+
+          <p>
+            nur vergiss nicht deine Freunde an der S-Bahn und vergiss nicht
+            deine Freunde in der Bar ja morgen ist ein neuer Tag und es bleibt
+            alles so, wie es war eine Tr&auml;ne kein Rekrut
+          </p>
+
+          <p>
+            Man geht nicht ohne Folgen unter Palmen wenn man nur handelt, nach
+            Bedarf verb&uuml;ndet im Versagen dieselben Kinder, andre Namen man
+            redet von der Liebe, aber spricht dabei von sich vom tiefen Stich im
+            K&ouml;rper, nur den K&ouml;rper sieht man nicht also kn&ouml;pf dir
+            deine Hemden zu und scheitle deine Stirn denn wer wei&szlig; denn
+            heutzutage noch wohin alle seine Blumen f&uuml;hren Ins offene Herz
+            dr&auml;ngt der Himmel und er fragt sich, was er da soll wie jemand,
+            der zu Waffen greift und vergessen hat, was er damit wollt
+          </p>
+
+          <p>
+            steh fr&uuml;h auf und gehe auf den Markt versuch doch mal spazieren
+            gehen im Park vertrete dir die Seele, sie wird gr&ouml;&szlig;er als
+            geahnt kratz die Totenkopftapete von der Wand nur vergiss nicht
+            deine Freunde an der S-Bahn und vergiss nicht deine Freunde in der
+            Bar ja morgen ist ein neuer Tag und es bleibt alles so, wie es war
+            eine Tr&auml;ne kein Rekrut
+          </p>
+
+          <p>
+            Mancher sucht das Leben nur, ums wieder zu vergessen andre kaufen
+            ein, nur um nichts davon zu essen versuch verschiedne Dinge, aber
+            sch&auml;m dich nicht dabei ein lebenswertes Leben ist erst
+            lebenswert von Scham befreit
+          </p>
+
+          <p>
+            gib besser schnell zur&uuml;ck, was du von fr&uuml;her noch
+            besa&szlig;t denn sie wollen alles wieder haben und mit Pech ist
+            nichts mehr da befleck dich nicht mit der Gewinnsucht, interessant
+            ist nur Verlust und wenn du auf den Friedhof gehst, dann sei dir
+            deines Grabs bewusst es gibt viele Gr&uuml;nde, f&uuml;r den Guten,
+            der nur schlechtes tut am anderen Ende der Sintflut eine Tr&auml;ne
+            kein Rekrut
+          </p>
+        </div>
+        <div id="fenster" className="text-container space-x-0 ml-14 mb-8">
+          <div className="text-2xl bold">Fenster zur Welt</div>
+          <p>
+            Lang in den N&auml;chten die Taxis fahren alle suchen was, ich
+            wei&szlig; nicht, was es gibt ohne Ged&auml;chtnis vom eigenen
+            Verfall die Antwort h&ouml;ren wollen woran einem etwas liegt
+          </p>
+
+          <p>
+            du sagst es geht dir besser heut als gestern aber morgen ist nicht
+            allzu weit entfernt nicht weiter dr&uuml;ber nachgedacht ist besser
+            leicht verdrossen ist das Fenster zur Welt
+          </p>
+
+          <p>
+            und die Bl&auml;tter der B&auml;ume fallen vor meinen Augen wir
+            klettern auf Kreuze, dass man es besser sieht die Bl&auml;tter der
+            B&auml;ume fallen vor meinen Augen weil es nichts besseres gibt
+          </p>
+
+          <p>
+            und wo die Sonne scheint, ist ganz egal denn um aufzugeben ist man
+            nie zu alt in die gl&auml;serne Leere nichts gefragt verstehen kann
+            nur, wer nicht die Wahrheit sagt verstehen kann nur, wer nicht die
+            Wahrheit sagt
+          </p>
+
+          <p>
+            frag mich nicht, wie es mir geht ich hab zu oft daran gedacht ich
+            w&uuml;nscht, ich h&auml;tt mein Leben mehr dem Leben hingegeben was
+            auch immer das bedeuten mag
+          </p>
+
+          <p>
+            da wo die Stadt ihren Schmerz lagert in schalen K&uuml;ssen leerer
+            Flaschen da tr&auml;um ich von Gewalt f&uuml;hle den, der trinkt,
+            und die Laterne leuchtet blind auf eine Stra&szlig;e, auf die
+            niemand sich mehr wagt
+          </p>
+
+          <p>
+            und die Bl&auml;tter der B&auml;ume fallen vor meinen Augen wir
+            klettern auf Kreuze, dass man es besser sieht die Bl&auml;tter der
+            B&auml;ume fallen vor meinen Augen wir klettern auf Kreuze, dass man
+            es besser sieht die Bl&auml;tter der B&auml;ume fallen vor meinen
+            Augen weil es nichts besseres gibt
+          </p>
+
+          <p>
+            lang in den N&auml;chten die taxis fahren und alle suchen, ich
+            versteh nicht was es gibt ohne Ged&auml;chtnis, nicht gelernt und
+            nicht verstanden woran einem eigentlich etwas liegt
+          </p>
+
+          <p>
+            ich frag mich, ob man sehn kann, dass ich l&auml;chle denn man fragt
+            mich, ob mir &uuml;berhaupt irgendwas gef&auml;llt nicht weiter
+            dr&uuml;ber nachgedacht ist besser eicht verdrossen bleibt das
+            Fenster zur Welt
+          </p>
+        </div>
+        <div id="wagen" className="text-container space-x-0 ml-14 mb-8">
+          <div className="text-2xl bold">Hoch auf dem gelben Wagen</div>
+          <p>
+            Es weht ein kalter Wind im Neubaugebiet, aber dennoch gehen die
+            Leute auf der Stra&szlig;e mir ist nicht mehr ganz klar, was man
+            hier gewinnt und die Sirenen singen mir Lieder vom Schlafen
+          </p>
+
+          <p>
+            Man unterhielt sich mit mir vom Balkon aus und ich stand dabei im
+            Garten das Gras war sch&ouml;n und es war frisch gem&auml;ht ich
+            wei&szlig; nicht, ob sie mir das &uuml;bel nahmen
+          </p>
+
+          <p>
+            An Orten, wo nur Stille herrscht, da nimmt man sich in Acht und
+            bel&auml;stigt besser kein mit seinen Fragen. Wo
+            Gem&auml;&szlig;igkeit spazieren geht, da horcht man, wenn gerufen
+            wird so f&uuml;hr ein kleines Buch mit deinen Taten Und am
+            sch&ouml;nsten aller Tage, ja da lauf ich euch davon die Sonne
+            w&auml;rmt die Haut, wenn ich entschlafe niemand, der von gro&szlig;
+            und wichtig spricht meint, dass es das wirklich gibt so f&uuml;rchte
+            nicht den Tod, sondern die Sprache
+          </p>
+
+          <p>
+            Man f&auml;hrt noch nicht so lang mit mir hoch auf dem gelben Wagen
+            es gibt so viele Dinge die andre schon gesehen haben ich malt mir
+            viele Bilder aus in v&ouml;llig andren Farben auch daf&uuml;r gibt
+            es Orte nur was soll man schon erwarten
+          </p>
+
+          <p>
+            Man f&auml;hrt noch nicht so lang mit mir hoch auf dem gelben Wagen
+            wer will schon jung und weise sein was soll man da schon sagen
+          </p>
+
+          <p>
+            Ich dachte ja auch, man versteht sich aber nun geb ich meinen
+            Anspruch zur&uuml;ck vielleicht wars zu viel, oder zu wenig aber wer
+            Verr&uuml;ckten in die Karten schaut ist selbst verr&uuml;ckt
+          </p>
+
+          <p>
+            Man h&auml;tte sich ja so viel zu sagen nur leider sieht man sich so
+            selten jetzt sind die Ziele vergessen und der Wille verfehlt aber
+            man f&auml;hrt hoch auf dem gelben Wagen
+          </p>
+
+          <p>
+            Und alle Stimmen der Vernunft singen in einem Chor aber warten
+            eigentlich nur auf ihre Pause im Gedachten war es gut und nur im
+            Echten tuts nicht Not h&auml;tt ich wissen sollen, als ich noch
+            daran glaubte denn am sch&ouml;nsten aller Tage da lauf ich euch
+            davon die Sonne w&auml;rmt die Haut, wenn ich entschlafe niemand,
+            der von gro&szlig; und wichtig spricht meint, dass es das wirklich
+            gibt so f&uuml;rchte nicht den Tod, sondern die Sprache
+          </p>
+        </div>
       </div>
     </Layout>
   )
@@ -415,10 +608,7 @@ export default Lyrics
 
 export const pageQuery = graphql`
   query AllLyrics {
-    allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "/lyrics/" } }
-      sort: { fields: [frontmatter___date], order: DESC }
-    ) {
+    allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/lyrics/" } }) {
       nodes {
         id
         frontmatter {
