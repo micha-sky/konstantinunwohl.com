b149a406f6716b9fa673a94108d98b930f568288
Alex Michalsky <michalsky@env.kiev.ua>
2024-04-24 22:07:00 +0200

    refactor: Update header image sizes and remove unused code

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,10 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-import { useEffect } from "react"
 
 const Header = () => {
   const [hideHeader, setHideHeader] = React.useState(false)
 
-  /*const checkScroll = () => {
-    setHideHeader(window.pageYOffset > 100)
-  }
-
-  useEffect(() => {
-    window.addEventListener("scroll", checkScroll)
-    return () => {
-      window.removeEventListener("scroll", checkScroll)
-    }
-  }, [])*/
   return (
     <header
       className={`flex flex-col items-center justify-between  w-full mx-auto p-4 z-10 ${
@@ -43,7 +32,7 @@ const Header = () => {
             quality={95}
             formats={["auto", "webp", "avif"]}
             alt=""
-            className="w-10 sm:w-28 md:w-36 lg:w-48 2xl:w-50"
+            className="w-14  sm:w-28 md:w-36 lg:w-48 2xl:w-60"
           />
         </Link>
         <Link
@@ -63,9 +52,7 @@ const Header = () => {
             style={{ marginBottom: `var(--space-3)` }}
           />
         </Link>
-      </div>
 
-      <div className="flex space-x-6 links-container justify-between">
         <Link
           to="/kontakt"
           style={{
@@ -97,23 +84,6 @@ const Header = () => {
             quality={95}
             formats={["auto", "webp", "avif"]}
             alt="Konzerte"
-            className="w-10 sm:w-28 md:w-36 lg:w-48"
-            style={{ marginBottom: `var(--space-3)` }}
-          />
-        </Link>
-        <Link
-          to="/projekte"
-          style={{
-            fontSize: `var(--font-sm)`,
-            textDecoration: `none`,
-          }}
-        >
-          <StaticImage
-            src="../images/projekte.png"
-            loading="lazy"
-            quality={95}
-            formats={["auto", "webp", "avif"]}
-            alt="Projekte"
             className="w-10 sm:w-28 md:w-36 lg:w-48"
             style={{ marginBottom: `var(--space-3)` }}
           />
