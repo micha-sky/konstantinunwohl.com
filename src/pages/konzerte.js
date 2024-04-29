718bb92561ea87c5f0c3f2fd6bfe0c4d4101577d
Alex Michalsky <michalsky@env.kiev.ua>
2024-04-29 12:52:38 +0200

    add songkick widget

--- src/pages/konzerte.js
+++ src/pages/konzerte.js
@@ -1,35 +1,55 @@
-import * as React from "react"
 import { Link } from "gatsby"
-
+import React, { useEffect } from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
 import { graphql } from "gatsby"
 
+import { faSoundcloud } from "@fortawesome/free-brands-svg-icons"
+
 const Konzerte = ({ data }) => {
   const konzerte = data.allMarkdownRemark.nodes
   console.log(konzerte)
 
+  useEffect(() => {
+    const script = document.createElement("script")
+    script.src = "//widget-app.songkick.com/injector/9405399"
+    script.async = true
+    document.body.appendChild(script)
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
+
   return (
     <Layout>
       <div className={styles.textCenter}>
-        <h1 className="text-2xl font-bold mb-4">Aktuelle konzerte</h1>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `
-              <a href="https://www.songkick.com/artists/9405399" class="songkick-widget" data-theme="dark" 
-              data-track-button="on" data-detect-style="off" 
-              data-background-color="rgb(0,0,0,1)" data-font-color="rgb(255,255,255,1)"
-               data-button-bg-color="rgb(255,255,255,1)" data-button-text-color="rgb(0,0,0,1)"
-                data-locale="en" data-other-artists="on" data-share-button="on" 
-                data-country-filter="on" 
-                data-rsvp="on" data-request-show="on" data-past-events="off" data-past-events-offtour="off" 
-                data-remind-me="off" style="display: none;"></a>
-              <script src='//widget-app.songkick.com/injector/9405399'></script>
-            `,
-          }}
-        />
-        <div className="flex flex-col md:flex-wrap md:justify-between">
+        <div className="max-w-screen-lg mx-auto p-4">
+          <a
+            href="https://www.songkick.com/artists/9405399"
+            className="songkick-widget"
+            data-theme="light"
+            data-track-button="on"
+            data-detect-style="off"
+            data-background-color="rgb(255,255,255,1)"
+            data-font-color="rgb(0,0,0,1)"
+            data-button-bg-color="rgb(0,0,0,1)"
+            data-button-text-color="rgb(255,255,255,1)"
+            data-locale="en"
+            data-other-artists="on"
+            data-share-button="on"
+            data-country-filter="on"
+            data-rsvp="on"
+            data-request-show="on"
+            data-past-events="off"
+            data-past-events-offtour="off"
+            data-remind-me="off"
+            style={{ display: "none" }}
+          ></a>
+          <script src="//widget-app.songkick.com/injector/9405399"></script>
+        </div>
+        <div className="flex flex-col md:flex-wrap md:justify-between hidden">
           {konzerte.map(konzert => (
             <div key={konzert.id} className=" p-4 rounded shadow mb-4 ">
               <h2 className="text-xl font-semibold mb-2">
