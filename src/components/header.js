26f63d2864027e7bdddaf5384ab572a7fe86fa5d
Alex Michalsky <michalsky@env.kiev.ua>
2024-10-03 23:38:00 +0200

    make header images consistent

--- src/components/header.js
+++ src/components/header.js
@@ -4,13 +4,14 @@ import { StaticImage } from "gatsby-plugin-image"
 
 const Header = () => {
   return (
-    <header className={`w-full mx-auto p-4 z-10`}>
-      <div className="flex justify-center">
-        <Link to="/" className="text-sm no-underline mb-3">
+    <header className="w-full mx-auto p-4 z-10">
+      <div className="flex justify-center mb-6">
+        <Link to="/" className="text-sm no-underline">
           <StaticImage
             src="../images/logo.png"
             loading="lazy"
             width={250}
+            height={75}
             quality={95}
             formats={["auto", "webp", "avif"]}
             alt="Konstantin Unwohl"
@@ -19,95 +20,79 @@ const Header = () => {
       </div>
 
       <div className="flex justify-center">
-        <div
-          className="grid
-    grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6
-    gap-0.5 sm:gap-1 md:gap-1.5 lg:gap-2 xl:gap-2
-    w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-7/12"
-        >
-          <Link
-            to="/musik"
-            className="text-sm no-underline transform scale-75 sm:scale-85 md:scale-100"
-          >
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-4 w-full max-w-4xl">
+          <Link to="/musik" className="text-sm no-underline block">
             <StaticImage
               src="../images/musik.png"
               loading="lazy"
+              width={150}
+              height={75}
               quality={95}
               formats={["auto", "webp", "avif"]}
               alt="Musik"
               className="w-full h-auto"
             />
           </Link>
-          <Link
-            to="/lyrics"
-            className="text-sm no-underline transform scale-75 sm:scale-85 md:scale-100"
-          >
+          <Link to="/lyrics" className="text-sm no-underline block">
             <StaticImage
               src="../images/lyrics.png"
               loading="lazy"
+              width={150}
+              height={75}
               quality={95}
               formats={["auto", "webp", "avif"]}
               alt="Lyrics"
-              height={75}
               className="w-full h-auto"
             />
           </Link>
-
-          <Link
-            to="/kontakt"
-            className="text-sm no-underline transform scale-75 sm:scale-85 md:scale-100"
-          >
+          <Link to="/kontakt" className="text-sm no-underline block">
             <StaticImage
               src="../images/kontakt.png"
               loading="lazy"
+              width={150}
+              height={75}
               quality={95}
               formats={["auto", "webp", "avif"]}
               alt="Kontakt"
-              height={75}
               className="w-full h-auto"
             />
           </Link>
-
-          <Link
-            to="/konzerte"
-            className="text-sm no-underline transform scale-75 sm:scale-85 md:scale-100"
-          >
+          <Link to="/konzerte" className="text-sm no-underline block">
             <StaticImage
               src="../images/konzerte.png"
               loading="lazy"
+              width={150}
+              height={75}
               quality={95}
               formats={["auto", "webp", "avif"]}
               alt="Konzerte"
-              height={75}
               className="w-full h-auto"
             />
           </Link>
-          <Link
-            to="/videos"
-            className="text-sm no-underline transform scale-75 sm:scale-85 md:scale-100"
-          >
+          <Link to="/videos" className="text-sm no-underline block">
             <StaticImage
               src="../images/videos.png"
               loading="lazy"
+              width={150}
+              height={75}
               quality={95}
               formats={["auto", "webp", "avif"]}
-              alt="Konzerte"
-              height={75}
+              alt="Videos"
               className="w-full h-auto"
             />
           </Link>
-
           <Link
             to="https://konstantinunwohl.bandcamp.com/merch"
-            className="text-sm no-underline transform scale-75 sm:scale-85 md:scale-100"
+            className="text-sm no-underline block"
           >
             <StaticImage
               src="../images/shop.png"
               loading="lazy"
+              width={150}
+              height={75}
               quality={95}
               formats={["auto", "webp", "avif"]}
               alt="Shop"
-              height={75}
               className="w-full h-auto"
             />
           </Link>
@@ -116,4 +101,5 @@ const Header = () => {
     </header>
   )
 }
+
 export default Header
