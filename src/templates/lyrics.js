30d0209261f2abb94362800bab115f57431ef326
Alex Michalsky <michalsky@env.kiev.ua>
2024-11-03 17:02:49 +0100

    load lyrics from md, correct styles

--- src/templates/lyrics.js
+++ src/templates/lyrics.js
@@ -2,20 +2,17 @@ import * as React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const LyricTemplate = ({ data }) => {
-  const { markdownRemark } = data
-  const { frontmatter, html } = markdownRemark
-
-  // Create state for track titles and modified HTML
+const LyricTemplate = ({ data: { site, markdownRemark: post }, location }) => {
+  const siteTitle = site.siteMetadata?.title || `Title`
   const [trackTitles, setTrackTitles] = React.useState([])
-  const [modifiedHtml, setModifiedHtml] = React.useState(html)
+  const [modifiedHtml, setModifiedHtml] = React.useState(post.html)
 
-  // Run DOM parsing only on the client side inside useEffect
   React.useEffect(() => {
     if (typeof window !== "undefined") {
       const parser = new DOMParser()
-      const doc = parser.parseFromString(html, "text/html")
+      const doc = parser.parseFromString(post.html, "text/html")
 
       const extractedTrackTitles = Array.from(doc.querySelectorAll("h2")).map(
         h2 => ({
@@ -25,15 +22,14 @@ const LyricTemplate = ({ data }) => {
       )
       setTrackTitles(extractedTrackTitles)
 
-      const updatedHtml = html.replace(/<h2>(.*?)<\/h2>/g, (match, p1) => {
+      const updatedHtml = post.html.replace(/<h2>(.*?)<\/h2>/g, (match, p1) => {
         const id = p1.toLowerCase().replace(/\s+/g, "-")
-        return `<h2 class="text-2xl" id="${id}">${p1}</h2>`
+        return `<h2 class="track-title" id="${id}"><strong>${p1}</strong></h2>`
       })
       setModifiedHtml(updatedHtml)
     }
-  }, [html])
+  }, [post.html])
 
-  // Function to scroll to a specific track
   const scrollToTrack = id => {
     if (typeof window !== "undefined") {
       const element = document.getElementById(id)
@@ -44,47 +40,87 @@ const LyricTemplate = ({ data }) => {
   }
 
   return (
-    <Layout>
-      <h1 className="text-4xl font-bold text-center my-6">
-        {frontmatter.title}
-      </h1>
+    <Layout location={location} title={siteTitle}>
+      <div className="flex flex-col main-container">
+        <article
+          className="blog-post"
+          itemScope
+          itemType="http://schema.org/Article"
+        >
+          <header>
+            <h1 className="main-title" itemProp="headline">
+              {post.frontmatter.title}
+            </h1>
+          </header>
 
-      {/* Track list with scroll buttons */}
-      <ul className="flex flex-col  space-y-4 mb-10">
-        {trackTitles.map((track, index) => (
-          <li key={index}>
-            <button
-              onClick={() => scrollToTrack(track.id)}
-              className="px-4 py-2 bg-blue-500 text-black rounded-lg shadow-md hover:bg-blue-600 focus:outline-none"
-            >
-              {track.title}
-            </button>
-          </li>
-        ))}
-      </ul>
+          <GatsbyImage
+            image={getImage(post.frontmatter.image)}
+            alt={post.frontmatter.title}
+            className="w-48 sm:w-48 md:w-36 lg:w-48 2xl:w-50"
+          />
 
-      {/* Lyrics content with modified HTML */}
-      <div
-        className="prose prose-lg prose-blue max-w-none mx-auto"
-        dangerouslySetInnerHTML={{ __html: modifiedHtml }}
-      />
+          {/* Track list with scroll buttons */}
+          <ol className="list-decimal list-inside">
+            {trackTitles.map((track, index) => (
+              <li key={index}>
+                <button
+                  onClick={() => scrollToTrack(track.id)}
+                  className="track-button"
+                >
+                  <p>{track.title}</p>
+                </button>
+              </li>
+            ))}
+          </ol>
+
+          {/* Lyrics content with modified HTML */}
+          <section
+            className="lyrics-content"
+            dangerouslySetInnerHTML={{ __html: modifiedHtml }}
+            itemProp="articleBody"
+          />
+          <hr />
+          <footer></footer>
+        </article>
+      </div>
     </Layout>
   )
 }
 
-export const Head = ({ data }) => (
-  <Seo title={data.markdownRemark.frontmatter.title} />
+export const Head = ({ data: { markdownRemark: post } }) => (
+  <Seo
+    title={post.frontmatter.title}
+    description={post.frontmatter.description || post.excerpt}
+  />
 )
 
 export default LyricTemplate
 
 export const pageQuery = graphql`
-  query ($id: String!) {
+  query LyricsBySlug($id: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     markdownRemark(id: { eq: $id }) {
+      id
+      excerpt(pruneLength: 160)
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
         title
+        description
+        image {
+          childImageSharp {
+            gatsbyImageData(
+              width: 200
+              height: 200
+              layout: CONSTRAINED
+              placeholder: BLURRED
+              formats: [AUTO, WEBP, AVIF]
+            )
+          }
+        }
       }
     }
   }
