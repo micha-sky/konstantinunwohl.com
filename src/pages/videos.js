a71466b35acc13188b7bc0c49d26ac92424c91b5
isidorfrohlich <160486061+isidorfrohlich@users.noreply.github.com>
2025-07-04 09:21:20 +0200

    Update videos.js

--- src/pages/videos.js
+++ src/pages/videos.js
@@ -9,6 +9,7 @@ const Videos = () => (
   <Layout>
     <div className={styles.textCenter}>
       <div className="flex flex-col py-4 space-y-6 text-center">
+        <YouTubeVideo videoId="Axufrbp4ARQ" />
         <YouTubeVideo videoId="64fxxIsMMLg" />
         <YouTubeVideo videoId="m3tHv4_zcV8" />
         <YouTubeVideo videoId="rscYO7mpwEo" />
